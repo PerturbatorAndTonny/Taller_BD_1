7c542852c84ad6a4b2c189d43ff7a63470b6a0a3
PerturbatorAndTonny <perturbatorandtonny@gmail.com>
2026-02-03 19:07:15 -0500

    create new article check

---
+++ controller/article.js
@@ -0,0 +1,11 @@
+import { newArticle } from  '../model/db/article.js'
+
+export const createArticle = async(req, res) => {
+  try {
+    await newArticle(req.body);
+    res.redirect("/article")
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error al crear usuario");
+  }
+}
